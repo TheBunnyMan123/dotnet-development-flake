0d6d68d60e7d71ddbe313cf2a3c0c587738e3ac1
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-05 14:56:42 -0500

    Initial

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A dev environment for Figura";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        system = "${system}";
+      };
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          bashInteractive
+          dotnetCorePackages.dotnet_8.sdk
+          (vscode-with-extensions.override {
+            vscode = vscodium;
+            vscodeExtensions = with vscode-extensions; [
+              sumneko.lua
+            ];
+          })
+        ];
+
+        shellHook = ''
+          exec codium --verbose .
+        '';
+      };
+    }
+  );
+
+  devShells = {
+    noIde = pkgs.mkShell {
+      packages = with pkgs; [
+        dotnetCorePackages.dotnet_8.sdk
+      ];
+    };
+  }
+}
